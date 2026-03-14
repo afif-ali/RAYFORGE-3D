403406a441866d45f0c1b999fa22c0111adc507f
Mohamed Ali Afif <me.ali.afif@gmail.com>
2026-03-14 15:26:06 +0000

    setup opengl

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow* window;
+    window = glfwCreateWindow(640, 480, "RAYFORGE 3D", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress))
+        return -1;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
